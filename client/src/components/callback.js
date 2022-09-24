e24f2214e0dd223a49ae98bafb441b8fa45ec291
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2022-09-24 17:31:32 +0530

    All done

---
+++ client/src/components/callback.js
@@ -0,0 +1,55 @@
+import { useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { magic } from '../lib/magic';
+import { UserContext } from '../lib/UserContext';
+import Loading from './loading';
+
+const Callback = (props) => {
+  console.log(props.location.search);
+  const history = useHistory();
+  const [user, setUser] = useContext(UserContext);
+
+  // The redirect contains a `provider` query param if the user is logging in with a social provider
+  useEffect(() => {
+    let provider = new URLSearchParams(props.location.search).get('provider');
+    console.log(provider);
+    provider ? finishSocialLogin() : finishEmailRedirectLogin();
+  }, [props.location.search]);
+
+  // `getRedirectResult()` returns an object with user data from Magic and the social provider
+  const finishSocialLogin = async () => {
+    let result = await magic.oauth.getRedirectResult();
+    authenticateWithServer(result.magic.idToken);
+    console.log(result,"result");
+  };
+
+  // `loginWithCredential()` returns a didToken for the user logging in
+  const finishEmailRedirectLogin = () => {
+    let magicCredential = new URLSearchParams(props.location.search).get('magic_credential');
+    console.log(magicCredential)
+    if (magicCredential)
+      magic.auth.loginWithCredential().then((didToken) => authenticateWithServer(didToken));
+  };
+
+  // Send token to server to validate
+  const authenticateWithServer = async (didToken) => {
+    let res = await fetch(`${process.env.REACT_APP_SERVER_URL}/api/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + didToken,
+      },
+    });
+    if (res.status === 200) {
+      // Set the UserContext to the now logged in user
+      let userMetadata = await magic.user.getMetadata();
+      await setUser(userMetadata);
+      console.log(userMetadata)
+      history.push('/profile');
+    }
+  };
+
+  return <Loading />;
+};
+
+export default Callback;
